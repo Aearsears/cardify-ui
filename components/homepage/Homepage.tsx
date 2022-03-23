660adae6513d535c562445da610fc9cede3a1f7e
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-23 18:23:46 -0400

    started on homepage

--- components/homepage/Homepage.tsx
+++ components/homepage/Homepage.tsx
@@ -22,12 +22,25 @@ function Homepage(props) {
                 <Typography variant="h2" className="text-center">
                     Turn your notes into study cards in a flash
                 </Typography>
+                <div>left: icon</div>
+                <div>some text</div>
             </div>
             <Divider light variant="middle" />
             <div className="section h-96  mt-4">
                 <Typography variant="h2" className="text-center ">
                     Turn your notes into study cards in a flash
                 </Typography>
+                <div>left: text</div>
+                <div>right: icon</div>
+            </div>
+            <Divider light variant="middle" />
+            <div className="section h-96  mt-4">
+                <Typography variant="h2" className="text-center ">
+                    Turn your notes into study cards in a flash
+                </Typography>
+                <div>icon and text</div>
+                <div>icon and text</div>
+                <div>icon and text</div>
             </div>
         </div>
     );
