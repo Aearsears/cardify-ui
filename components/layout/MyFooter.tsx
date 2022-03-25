fb9385ae8db844d171306d648e9770a2d01595d1
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-24 23:13:43 -0400

    started on footer

--- components/layout/MyFooter.tsx
+++ components/layout/MyFooter.tsx
@@ -4,7 +4,38 @@ function MyFooter(props) {
     return (
         <footer>
             <hr />
-            <span>I'm here to stay (Footer)</span>
+            <div className="social">
+                <a href="#">
+                    <i className="icon ion-social-instagram"></i>
+                </a>
+                <a href="#">
+                    <i className="icon ion-social-snapchat"></i>
+                </a>
+                <a href="#">
+                    <i className="icon ion-social-twitter"></i>
+                </a>
+                <a href="#">
+                    <i className="icon ion-social-facebook"></i>
+                </a>
+            </div>
+            <ul className="list-inline">
+                <li className="list-inline-item">
+                    <a href="#">Home</a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">Services</a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">About</a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">Terms</a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">Privacy Policy</a>
+                </li>
+            </ul>
+            <p className="copyright">Cardify © 2022</p>
         </footer>
     );
 }
