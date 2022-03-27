0f55e675ac7c643111e22ff3ba52065c431e679c
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-27 16:08:59 -0400

    refactored flashcard and made whole card clickable

--- components/flashcard/flashcard.tsx
+++ components/flashcard/flashcard.tsx
@@ -1,9 +1,7 @@
 import { CardActionArea, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
 
@@ -16,20 +14,26 @@ interface Props {
 const innerCard = (
     text: string,
     handleClick: (event: React.MouseEvent<HTMLElement>) => void,
+    width: number = 300,
+    height: number = 300,
     context?: string
 ) => (
-    <React.Fragment>
-        <CardContent>
-            <Typography variant="h5" component="div">
-                {text}
-            </Typography>
-            {context ? (
-                <Typography variant="h6" component="div">
-                    {context}
-                </Typography>
-            ) : null}
-        </CardContent>
-    </React.Fragment>
+    <Box sx={{ width: width, height: height }}>
+        <Card variant="elevation" className="w-full h-full">
+            <CardActionArea onClick={handleClick} className="h-full w-full">
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {text}
+                    </Typography>
+                    {context ? (
+                        <Typography variant="h6" component="div">
+                            {context}
+                        </Typography>
+                    ) : null}
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    </Box>
 );
 
 function Flashcard(props: Props) {
@@ -40,23 +44,14 @@ function Flashcard(props: Props) {
     };
     return (
         <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-            <Box>
-                <Card variant="elevation" sx={{ width: 200, height: 200 }}>
-                    <CardActionArea
-                        onClick={handleClick}
-                        className="w-full h-full"
-                    >
-                        {innerCard(props.question, handleClick)}
-                    </CardActionArea>
-                </Card>
-            </Box>
-            <Box>
-                <Card variant="elevation" sx={{ width: 200, height: 200 }}>
-                    <CardActionArea onClick={handleClick}>
-                        {innerCard(props.answer, handleClick, props.context)}
-                    </CardActionArea>
-                </Card>
-            </Box>
+            {innerCard(props.question, handleClick)}
+            {innerCard(
+                props.answer,
+                handleClick,
+                undefined,
+                undefined,
+                props.context
+            )}
         </ReactCardFlip>
     );
 }
