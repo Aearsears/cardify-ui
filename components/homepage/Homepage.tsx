56f8db53f7841508bca67434ad6632b1a021b030
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-16 15:26:05 -0400

    lightning overlay works

--- components/homepage/Homepage.tsx
+++ components/homepage/Homepage.tsx
@@ -17,140 +17,15 @@ import SettingsSharpIcon from '@mui/icons-material/SettingsSharp';
 import SendIcon from '@mui/icons-material/Send';
 import Emoji from '../Emoji';
 import HPCodeBlock from './HPCodeBlock';
+import Lightning from './Lightning';
 
 //https://codepen.io/SoorajSnBlz/pen/LYZGjBj
-class Lightning {
-    start: {};
-    end: {};
-    thickness: number;
-    opacity: number;
-    constructor(x1, y1, x2, y2, thickness, opacity) {
-        this.start = { x1, y1 };
-        this.end = { x2, y2 };
-        this.thickness = thickness;
-        this.opacity = opacity;
-    }
-    draw() {
-        return line(this.start, this.end, this.thickness, this.opacity);
-    }
-
-    static getRandomFloat = function (min, max) {
-        const random = Math.random() * (max - min + 1) + min;
-        return random;
-    };
-
-    static getRandomInteger = function (min, max) {
-        return Math.floor(Lightning.getRandomFloat(min, max));
-    };
-}
-
-function lightningAnim() {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    const lightningStrikeOffset = 5;
-    const lightningStrikeLength = 100;
-    const lightningBoltLength = 5;
-    const lightningThickness = 4;
-    const canvasHeight = 200;
-    const canvasWidth = 200;
-
-    const clearCanvas = function (x, y, height, width) {
-        rectX = x || 0;
-        rectY = y || 0;
-        rectHeight = height || canvasHeight;
-        rectWidth = width || canvasWidth;
-        context.clearRect(rectX, rectY, rectWidth, rectHeight);
-        context.beginPath();
-    };
-
-    const line = function (start, end, thickness, opacity) {
-        context.beginPath();
-        context.moveTo(start.x, start.y);
-        context.lineTo(end.x, end.y);
-        context.lineWidth = thickness;
-        context.strokeStyle = `rgba(255, 255, 255, ${opacity})`;
-        context.shadowBlur = 30;
-        context.shadowColor = '#bd9df2';
-        context.stroke();
-        context.closePath();
-    };
-
-    const interval = 3000;
-    // const lightningStrikeOffset = 5;
-    // const lightningStrikeLength = 100;
-    // const lightningBoltLength = Math.random() * 10;
-    // const lightningThickness = 4;
-    let lightning = [];
-
-    const createLightning = function () {
-        lightning = [];
-        let lightningX1 = getRandomInteger(2, canvasWidth - 2);
-        let lightningX2 = getRandomInteger(
-            lightningX1 - lightningStrikeOffset,
-            lightningX1 + lightningStrikeOffset
-        );
-        lightning[0] = new Lightning(
-            lightningX1,
-            0,
-            lightningX2,
-            lightningBoltLength,
-            lightningThickness,
-            1
-        );
-        for (let l = 1; l < lightningStrikeLength; l++) {
-            let lastBolt = lightning[l - 1];
-            let lx1 = lastBolt.end.x;
-            let lx2 = getRandomInteger(
-                lx1 - lightningStrikeOffset,
-                lx1 + lightningStrikeOffset
-            );
-            lightning.push(
-                new Lightning(
-                    lx1,
-                    lastBolt.end.y,
-                    lx2,
-                    lastBolt.end.y + lightningBoltLength,
-                    lastBolt.thickness,
-                    lastBolt.opacity
-                )
-            );
-        }
-    };
-
-    const setup = function () {
-        createLightning();
-        for (let i = 0; i < lightning.length; i++) {
-            lightning[i].draw();
-        }
-    };
-
-    const animate = function () {
-        clearCanvas();
-
-        for (let i = 0; i < lightning.length; i++) {
-            lightning[i].opacity -= 0.01;
-            lightning[i].thickness -= 0.05;
-            if (lightning[i].thickness <= 2) {
-                lightning[i].end.y -= 0.05;
-            }
-            lightning[i].draw();
-        }
-
-        requestAnimationFrame(animate);
-    };
-
-    setup();
-    requestAnimationFrame(animate);
-    setInterval(function () {
-        createLightning();
-    }, interval);
-}
 
 function Homepage(props) {
     const { darkMode } = useContext(ColourModeContext);
     return (
         <div className="block">
-            <div className={`${styles.bg} p-4`}>
+            <div className={`${styles.bg} p-4 relative`}>
                 <Typography variant="h2" className="text-center ">
                     Turn your notes into study cards in a flash
                 </Typography>
@@ -158,7 +33,7 @@ function Homepage(props) {
                     <HPAnimation></HPAnimation>
                     <HPCodeBlock></HPCodeBlock>
                 </div>
-                <canvas id="canvas"></canvas>
+                <Lightning className="w-full h-full absolute top-0"></Lightning>
             </div>
             <Divider light variant="middle" />
             <div className={`${styles.section} h-full mt-4 flex items-center`}>
---
+++ components/homepage/Lightning.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Lightning.module.css';
+
+Lightning.propTypes = {};
+
+const getRandomInteger = function (min, max) {
+    return Math.floor(getRandomFloat(min, max));
+};
+
+const getRandomFloat = function (min, max) {
+    const random = Math.random() * (max - min + 1) + min;
+    return random;
+};
+
+interface Lightning {
+    start: { x: number; y: number };
+    end: { x: number; y: number };
+    thickness: number;
+    opacity: number;
+    draw: (
+        start: { x: number; y: number },
+        end: { x: number; y: number },
+        thickness: number,
+        opacity: number
+    ) => void;
+}
+interface Props {
+    className?: string;
+}
+function Lightning(props: Props) {
+    let ref = useRef<HTMLCanvasElement>();
+
+    useEffect(() => {
+        let canvas = ref.current;
+        let context = canvas.getContext('2d');
+        let lightning: Lightning[] = [];
+        const lightningStrikeOffset = 5;
+        const lightningStrikeLength = 100;
+        const lightningBoltLength = 5;
+        const lightningThickness = 4;
+        const interval = 1000;
+        const canvasHeight = 500;
+        const canvasWidth = 500;
+
+        const clearCanvas = function (
+            x?: number,
+            y?: number,
+            height?: number,
+            width?: number
+        ) {
+            let rectX = x || 0;
+            let rectY = y || 0;
+            let rectHeight = height || canvasHeight;
+            let rectWidth = width || canvasWidth;
+            context.clearRect(rectX, rectY, rectWidth, rectHeight);
+            context.beginPath();
+        };
+
+        const line = function (
+            start: { x: number; y: number },
+            end: { x: number; y: number },
+            thickness: number,
+            opacity: number
+        ) {
+            context.beginPath();
+            context.moveTo(start.x, start.y);
+            context.lineTo(end.x, end.y);
+            context.lineWidth = thickness;
+            context.strokeStyle = `rgba(255, 255, 255, ${opacity})`;
+            context.shadowBlur = 30;
+            context.shadowColor = '#bd9df2';
+            context.stroke();
+            context.closePath();
+        };
+        const createLightning = function () {
+            lightning = [];
+            let lightningX1 = getRandomInteger(2, canvasWidth - 2);
+            let lightningX2 = getRandomInteger(
+                lightningX1 - lightningStrikeOffset,
+                lightningX1 + lightningStrikeOffset
+            );
+            lightning[0] = {
+                start: { x: lightningX1, y: 0 },
+                end: { x: lightningX2, y: lightningBoltLength },
+                thickness: lightningThickness,
+                opacity: 1,
+                draw: line
+            };
+            for (let l = 1; l < lightningStrikeLength; l++) {
+                let lastBolt = lightning[l - 1];
+                let lx1 = lastBolt.end.x;
+                let lx2 = getRandomInteger(
+                    lx1 - lightningStrikeOffset,
+                    lx1 + lightningStrikeOffset
+                );
+                lightning.push({
+                    start: {
+                        x: lx1,
+
+                        y: lastBolt.end.y
+                    },
+                    end: { x: lx2, y: lastBolt.end.y + lightningBoltLength },
+                    thickness: lastBolt.thickness,
+                    opacity: lastBolt.opacity,
+                    draw: line
+                });
+            }
+        };
+        const setup = function () {
+            createLightning();
+            for (let i = 0; i < lightning.length; i++) {
+                let l = lightning[i];
+                l.draw(l.start, l.end, l.thickness, l.opacity);
+            }
+        };
+
+        const animate = function () {
+            clearCanvas();
+
+            for (let i = 0; i < lightning.length; i++) {
+                lightning[i].opacity -= 0.01;
+                lightning[i].thickness -= 0.05;
+                if (lightning[i].thickness <= 2) {
+                    lightning[i].end.y -= 0.05;
+                }
+                let l = lightning[i];
+                l.draw(l.start, l.end, l.thickness, l.opacity);
+            }
+
+            requestAnimationFrame(animate);
+        };
+
+        setup();
+        requestAnimationFrame(animate);
+        setInterval(function () {
+            createLightning();
+        }, interval);
+    });
+    return (
+        <canvas
+            ref={ref}
+            className={`${props.className} ${styles.lightning}`}
+        ></canvas>
+    );
+}
+
+export default Lightning;
