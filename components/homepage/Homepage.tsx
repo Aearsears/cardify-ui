7db0a71e7324c639570f4d41ee86092db02f7d35
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-20 11:41:50 -0400

    formatted homepage for more professional and consistent spacing

--- components/homepage/Homepage.tsx
+++ components/homepage/Homepage.tsx
@@ -23,114 +23,125 @@ function Homepage(props) {
     const { darkMode } = useContext(ColourModeContext);
     return (
         <div className="block">
-            <div className={`${styles.bg} p-4 relative h-96`}>
-                <div className="mb-4">
-                    <Typography variant="h2" className="text-center">
-                        Turn your notes into study cards in a flash
-                        <Emoji label="lightning" symbol="⚡"></Emoji>
-                    </Typography>
-                </div>
-                <div className="grid grid-flow-row grid-cols-2">
-                    <HPAnimation></HPAnimation>
-                    <HPCodeBlock></HPCodeBlock>
-                </div>
-                <Lightning className="w-full h-full absolute top-0 right-0"></Lightning>
-            </div>
-            <Divider light variant="middle" />
-            <div className={`${styles.section} h-full mt-4 flex items-center`}>
-                <div className="w-6/12 h-6/12 flex items-center justify-center">
-                    <Image
-                        src={darkMode ? exam_dark : exam_light}
-                        placeholder="blur"
-                    ></Image>
-                </div>
-                <div className="text">
-                    <Typography variant="h3" className="text-center">
-                        Study more efficiently by leveraging state of the art
-                        A.I.
-                    </Typography>
-                    <Typography variant="h6" className="text-center">
-                        Save time, money and beat the competition.
-                    </Typography>
-                </div>
-            </div>
-            <Divider light variant="middle" />
-            <div className={`${styles.section} h-full mt-4 flex items-center`}>
-                <div className="text">
-                    <Typography variant="h3" className="text-center">
-                        Proven results. Great satification.
-                    </Typography>
-                    <Typography variant="h6" className="text-center">
-                        Get the results you want. <br></br>Flashcard will help
-                        you retain up to 75% more information.
-                    </Typography>
-                </div>
-                <div className="w-6/12 h-6/12 flex items-center justify-center">
-                    <Image
-                        src={darkMode ? results_dark : results_light}
-                        height="1200"
-                        width="1600"
-                        placeholder="blur"
-                    ></Image>
-                </div>
-            </div>
-            <Divider light variant="middle" />
-            <div className="section h-full mt-4">
-                <div className="text">
-                    <Typography variant="h3" className="text-center">
-                        Cardify's ecosystem synergizes well with students and
-                        professors alike.
-                    </Typography>
-                </div>
-                <div className="flex items-center">
-                    <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
-                        <BoltSharpIcon
-                            sx={{ fontSize: 350 }}
-                            color="secondary"
-                        ></BoltSharpIcon>
-                        <Typography className="text-center" component="div">
-                            Lightning fast creation of flash card so you are up
-                            and running in seconds.
+            <div className={`${styles.bg} p-4 relative`}>
+                <div className="container mr-auto ml-auto">
+                    <div className="mb-4">
+                        <Typography variant="h2" className="text-center">
+                            Turn your notes into study cards in a flash
+                            <Emoji label="lightning" symbol="⚡"></Emoji>
                         </Typography>
                     </div>
-                    <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
-                        <BorderColorSharpIcon
-                            sx={{ fontSize: 350 }}
-                            color="secondary"
-                        ></BorderColorSharpIcon>
-                        <Typography className="text-center" component="div">
-                            Focus on taking high quality notes.
-                        </Typography>
+                    <div className="grid grid-flow-row grid-cols-2">
+                        <HPAnimation></HPAnimation>
+                        <HPCodeBlock></HPCodeBlock>
                     </div>
-                    <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
-                        <SettingsSharpIcon
-                            sx={{ fontSize: 350 }}
-                            color="secondary"
-                        ></SettingsSharpIcon>
-                        <Typography className="text-center" component="div">
-                            Easy setup and supports multiple study decks.
+                    <Lightning className="w-full h-full absolute top-0 right-0"></Lightning>
+                </div>
+            </div>
+            <Divider light variant="middle" />
+            <div className={`${styles.section} h-full p-4`}>
+                <div className="container mr-auto ml-auto">
+                    <div className="w-6/12 h-6/12 inline-block float-none align-middle">
+                        <Image
+                            src={darkMode ? exam_dark : exam_light}
+                            placeholder="blur"
+                        ></Image>
+                    </div>
+                    <div className="text w-6/12 h-6/12 inline-block float-none align-middle">
+                        <Typography variant="h2" className="text-center">
+                            Study more efficiently by leveraging state of the
+                            art A.I.
+                        </Typography>
+                        <Typography variant="h6" className="text-center">
+                            Save time, money and beat the competition.
                         </Typography>
                     </div>
                 </div>
             </div>
             <Divider light variant="middle" />
-            <div className={`${styles.section} h-full mt-4`}>
-                <div className="text">
-                    <Typography variant="h3" className="text-center">
-                        Ready for{' '}
-                        <Box component="span" fontStyle="italic">
-                            speed?
-                        </Box>{' '}
-                        <Emoji label="car" symbol="🏎️"></Emoji>
-                    </Typography>{' '}
-                    <div className="flex items-center justify-center m-2">
-                        <Button
-                            variant="contained"
-                            className={`${styles.anim} text-center`}
-                            endIcon={<SendIcon></SendIcon>}
-                        >
-                            Sign up
-                        </Button>
+            <div className={`${styles.section} h-full p-4`}>
+                <div className="container mr-auto ml-auto">
+                    <div className="text w-6/12 h-6/12 inline-block float-none align-middle">
+                        <Typography variant="h2" className="text-center">
+                            Proven results. Great satification.
+                        </Typography>
+                        <Typography variant="h6" className="text-center">
+                            Get the results you want. <br></br>Flashcard will
+                            help you retain up to 75% more information.
+                        </Typography>
+                    </div>
+                    <div className="text w-6/12 h-6/12 inline-block float-none align-middle">
+                        <Image
+                            src={darkMode ? results_dark : results_light}
+                            height="1200"
+                            width="1600"
+                            placeholder="blur"
+                        ></Image>
+                    </div>
+                </div>
+            </div>
+
+            <Divider light variant="middle" />
+            <div className="h-full p-4">
+                <div className="container mr-auto ml-auto">
+                    <div className="text">
+                        <Typography variant="h3" className="text-center">
+                            Cardify's ecosystem synergizes well with students
+                            and professors alike.
+                        </Typography>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
+                            <BoltSharpIcon
+                                sx={{ fontSize: 350 }}
+                                color="secondary"
+                            ></BoltSharpIcon>
+                            <Typography className="text-center" component="div">
+                                Lightning fast creation of flash card so you are
+                                up and running in seconds.
+                            </Typography>
+                        </div>
+                        <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
+                            <BorderColorSharpIcon
+                                sx={{ fontSize: 350 }}
+                                color="secondary"
+                            ></BorderColorSharpIcon>
+                            <Typography className="text-center" component="div">
+                                Focus on taking high quality notes.
+                            </Typography>
+                        </div>
+                        <div className="w-6/12 h-6/12 flex items-center justify-center flex-wrap flex-row">
+                            <SettingsSharpIcon
+                                sx={{ fontSize: 350 }}
+                                color="secondary"
+                            ></SettingsSharpIcon>
+                            <Typography className="text-center" component="div">
+                                Easy setup and supports multiple study decks.
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Divider light variant="middle" />
+            <div className={`${styles.section} h-full p-4`}>
+                <div className="container mr-auto ml-auto">
+                    <div className="text">
+                        <Typography variant="h3" className="text-center">
+                            Ready for{' '}
+                            <Box component="span" fontStyle="italic">
+                                speed?
+                            </Box>{' '}
+                            <Emoji label="car" symbol="🏎️"></Emoji>
+                        </Typography>{' '}
+                        <div className="flex items-center justify-center m-2">
+                            <Button
+                                variant="contained"
+                                className={`${styles.anim} text-center`}
+                                endIcon={<SendIcon></SendIcon>}
+                            >
+                                Sign up
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
