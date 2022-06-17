50e5798bf7b4cb0e317d2c80940508b77574759e
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-06-17 16:59:14 -0400

    added character counter to ai dialog

--- components/studydeck/AddAICardDialog.tsx
+++ components/studydeck/AddAICardDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
     Button,
@@ -16,26 +16,40 @@ interface Props {
     setOpen: (state: boolean) => void;
 }
 function AddAICardDialog(props: Props) {
+    const [userInput, setUserInput] = useState('');
+
     const handleClose = () => {
         props.setOpen(false);
     };
+
+    const handleTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput(e.target.value);
+    };
+
     return (
         <div>
-            <Dialog open={props.open} onClose={handleClose}>
+            <Dialog open={props.open} onClose={handleClose} fullWidth>
                 <DialogTitle>A.I produced cards</DialogTitle>
                 <DialogContent>
                     <DialogContentText color="info">
-                        Enter in your text below.
+                        Enter in your text below. <br></br>
+                        Do not add more than 500 characters.
                     </DialogContentText>
                     <TextField
                         autoFocus
                         margin="dense"
                         id="name"
                         label="Text"
+                        type="text"
                         fullWidth
                         variant="standard"
                         color="error"
+                        value={userInput}
+                        onChange={handleTextFieldChange}
                     />
+                    <DialogContentText color="info">
+                        Current number of characters: {userInput.length}
+                    </DialogContentText>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="info">
